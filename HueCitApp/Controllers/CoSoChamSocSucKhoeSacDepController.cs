e1fab76494d300cda292ed223f2ac0b4d2bc09b1
tranrin <tranngocrin12@gmail.com>
2022-10-07 15:35:58 +0700

    RIn Seventh Commit

---
+++ HueCitApp/Controllers/CoSoChamSocSucKhoeSacDepController.cs
@@ -0,0 +1,28 @@
+﻿using Application.DiemVeSinh;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Threading;
+using Application.CoSoChamSocSucKhoeSacDep;
+
+namespace HueCitApp.Controllers
+{
+    public class CoSoChamSocSucKhoeSacDepController : BaseApiController
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public CoSoChamSocSucKhoeSacDepController(IWebHostEnvironment hostingEnvironment) : base(hostingEnvironment)
+        {
+            _webHostEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+
+        public async Task<IActionResult> danhsachdiemvesinh(CancellationToken ct)
+        {
+            var listResult = await Mediator.Send(new  DanhSachCoSoChamSocSucKhoeSacDep.Query(), ct);
+            return HandlerResult(listResult);
+        }
+    }
+}
