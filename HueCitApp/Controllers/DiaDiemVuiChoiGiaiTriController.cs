9c283774ddede4b731f4b38969987b87a7c86f34
tranrin <tranngocrin12@gmail.com>
2022-10-07 10:17:26 +0700

    Rin Seventh Commit

---
+++ HueCitApp/Controllers/DiaDiemVuiChoiGiaiTriController.cs
@@ -0,0 +1,27 @@
+﻿using Application.DuBaoThoiTiet;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Threading;
+using Application.DiaDiemMuaSamGiaiTri;
+
+namespace HueCitApp.Controllers
+{
+    public class DiaDiemVuiChoiGiaiTriController : BaseApiController
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public DiaDiemVuiChoiGiaiTriController(IWebHostEnvironment hostingEnvironment) : base(hostingEnvironment)
+        {
+            _webHostEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+
+        public async Task<IActionResult> DanhSachDuBao(CancellationToken ct)
+        {
+            var listResult = await Mediator.Send(new DanhSachDiaDiemMuaSamGiaiTri.Query(), ct);
+            return HandlerResult(listResult);
+        }
+    }
+}
