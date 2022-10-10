e4bf7eb2bdf3fe0e879baaf4ce2e5babf275b88c
tranrin <tranngocrin12@gmail.com>
2022-10-10 16:45:47 +0700

    Rin ninth commit

---
+++ Application/LoaiPhuongtienGiaoThong/LoaiPhuongTienGiaoThongGets.cs
@@ -0,0 +1,45 @@
+﻿using Application.Core;
+using Dapper;
+using Domain;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.LoaiPhuongtienGiaoThong
+{
+    public class LoaiPhuongTienGiaoThongGets
+    {
+        public class Query : IRequest<Result<List<DanhMuc>>>
+        {// su li tham so dau vao
+
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<DanhMuc>>>
+        {
+            private readonly IConfiguration _configuration;
+            public Handler(IConfiguration configuration)
+            {
+                _configuration = configuration;
+            }
+
+            public async Task<Result<List<DanhMuc>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                string spName = "LoaiPhuongTienGiaoThongGets";
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    connection.Open();
+                    var result = await connection.QueryAsync<DanhMuc>(new CommandDefinition(spName, parameters: null, commandType: System.Data.CommandType.StoredProcedure));
+                    return Result<List<DanhMuc>>.Success(result.ToList());
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Domain/DanhMuc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DanhMuc
+    {
+        public int Id { get; set; }
+        public string Ten { get; set; } 
+        public int LoaiId { get; set; } 
+        public string MoTa { get; set; }    
+        public bool Status { get; set; }    
+        public bool IsDelete { get; set; }
+
+    }
+}
