91a8b1ff0090db6d91ed32631569055a16a28730
tranrin <tranngocrin12@gmail.com>
2022-11-14 15:35:26 +0700

    Rin commit again

--- HueCitApp/Startup.cs
+++ HueCitApp/Startup.cs
@@ -19,7 +19,7 @@ using HueCitApp.MiddleWare;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
@@ -35,6 +35,7 @@ using Microsoft.Extensions.Options;
 using System.Configuration;
 using API.Services;
 using HueCitApp.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HueCitApp
 {
@@ -76,7 +77,7 @@ namespace HueCitApp
                     .ForJob(jobKey) // link to the HelloWorldJob
                     .WithIdentity("HelloWorldJob-trigger") // give the trigger a unique name
                     .WithDailyTimeIntervalSchedule((x =>
-           x.WithIntervalInSeconds(10).Build())));
+           x.WithIntervalInMinutes(42).Build())));
                 // run every 5 seconds
             });
 
@@ -116,7 +117,7 @@ namespace HueCitApp
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TravelApi", Version = "v1" });
             });
             services.Configure<MailSettings>(_config.GetSection("MailSettings"));
-            services.AddTransient<ImailService, MailService>();
+            services.AddSingleton<ImailService, MailService>();
 
 
 
